574c7840521268ae69a08f946f9b8ab0b5f51b03
Varun Singh <121516348+I-Varun@users.noreply.github.com>
2024-01-03 20:41:25 +0530

    Update TCPServer.java

--- src/socket/TCPServer.java
+++ src/socket/TCPServer.java
@@ -1,35 +1,65 @@
 package socket;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TCPServer {
 
     public void start() throws Exception{
-        String sentence;
-
+      
+    	System.out.println("Server waiting.......");
+    	
         ServerSocket welcomeSocket = new ServerSocket(3000);
 
         while(true){
 
             Socket connectionSocket = welcomeSocket.accept();
+            
+            System.out.println("Connection Established");
 
             BufferedReader inFromClient = new BufferedReader(new
 
                     InputStreamReader(connectionSocket.getInputStream()));
-
-            OutputStream outToClient = connectionSocket.getOutputStream();
-
-            sentence = inFromClient.readLine();
-
-
-
-            outToClient.write((sentence+"\n").getBytes());
-
+            
+            String content , message;
+            PrintWriter writer = new PrintWriter(connectionSocket.getOutputStream(), true);
+            
+            //Reading data from file sent by client
+            content = inFromClient.readLine();
+            
+            File file = new File(content);
+            
+            if (!file.exists() || !file.isFile()) {
+                System.out.println("Invalid path or file does not exist.");
+            }
+            try {
+                              
+            	 BufferedReader buf  = new BufferedReader(new FileReader(file));
+            	 
+                 String data;
+                 
+                 //Sending the read data as a response to client
+                 while ((data = buf.readLine()) != null) {
+                     writer.println(data);
+                 }
+                 buf.close();
+                 
+                 
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            
             connectionSocket.close();
+            
+
     }
 
     }
